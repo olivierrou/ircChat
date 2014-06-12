6f3daedb3c7c99ac8c8d7c541300fff6e599e7bc
Bertrand Wittmer <bwit@magnus.fr>
2014-06-12 15:46:03 +0200

    Test de commit : commentaire

--- Cs-Irc-UI/src/com/cfranc/irc/ui/SimpleChatClientApp.java
+++ Cs-Irc-UI/src/com/cfranc/irc/ui/SimpleChatClientApp.java
@@ -34,7 +34,7 @@ public class SimpleChatClientApp {
     int serverPort;
     User oUser;
     String serverName;
-
+// Commentaire pour test git
 	private SimpleChatFrameClient frame;
 	public StyledDocument documentModel=new DefaultStyledDocument();
 	DefaultListModel<String> clientListModel=new DefaultListModel<String>();
